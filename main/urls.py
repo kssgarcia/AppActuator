37e0a911735cc93bf6d0044d8e690eedd886a9b2
Kevin <santyagogarcia.777@gmail.com>
2021-11-29 16:26:24 -0500

    new thing 2

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("",views.Home, name="web-home"),
+    path("result/",views.HomeResult, name="web-result"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render, redirect
 from .forms import ActuatorForms 
 import KevinModule as kv
+from json import dumps
 
 # Create your views here.
 
 def Home(request):
+    dataActuatorJson = None
+    dataActuator = {
+            'dataXYOption': [],
+            'mapData': [],
+            'dataEfficiency': [],
+            'dataSearh': []
+            }
     if request.method == 'POST':
         form = ActuatorForms(request.POST)
         if form.is_valid():
             print("The forms send correctly")
-            print(form.cleaned_data.values())
-            actuator = kv.Actuator(*form.cleaned_data.values())
-            print(dir(actuator))
+            values = list(form.cleaned_data.values())
+            # Actuator call 
+            actuator = kv.Actuator(*values[0:11])
+            dataActuator['dataXYOption'] = actuator.SearchPosition(list(cleanLim(values[11])), list(cleanLim(values[12])), values[13], list(cleanLim(values[14])), list(cleanLim(values[15])), values[16])
+            dataActuator['mapData'] = actuator.Mapped()
+            dataActuator['dataEfficiency'] = actuator.Efficiency(0.3)
+            dataActuator['dataSearch'] = actuator.SearchResult()
+            dataActuatorJson = dumps(dataActuator)
 
     else:
         form = ActuatorForms()   
-    context = {'form': form}
+    context = {'form': form, 'data': dataActuatorJson}
     return render(request, 'main/home.html', context)
+
+def HomeResult(request, data):
+    data = dumps(data)
+    return render(request, 'main/home.html', data)
+
+
+def cleanLim(lim):
+    character = ["[", "]"]
+    for char in character:
+        lim = lim.strip(char)
+    return map(float, lim.split(","))
 
 
 # 0.278, 10000.0, 0.45, 0.520, 135.0, -1, 0.4, -4561.65, 0.1, 16, 0.01
